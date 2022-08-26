42a4df6b9e2100acba062495a16e72631103f4b6
Hongsugyeong <olivia258@naver.com>
2022-08-26 17:48:29 +0900

    boardFileDTO 추가

---
+++ src/main/java/com/gang/start/board/impl/boardFileDTO.java
@@ -0,0 +1,16 @@
+package com.gang.start.board.impl;
+
+import com.gang.start.file.FileDTO;
+
+public class boardFileDTO extends FileDTO{
+	
+	private Integer num;
+
+	public Integer getNum() {
+		return num;
+	}
+
+	public void setNum(Integer num) {
+		this.num = num;
+	}
+}
