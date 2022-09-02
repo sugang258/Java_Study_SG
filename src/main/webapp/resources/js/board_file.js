df3b709aa2a6f4ba1f8d36d393b862cda90f3210
Hongsugyeong <olivia258@naver.com>
2022-09-02 09:47:20 +0900

    file 삭제/갯수제한

--- src/main/webapp/resources/js/board_file.js
+++ src/main/webapp/resources/js/board_file.js
@@ -1,19 +1,25 @@
 const addFiles = document.getElementById("addFiles");
 const fileAdd = document.getElementById("fileAdd");
+
 let count = 0;
+let idx = 0;
+    
 fileAdd.addEventListener("click",function() {
     
-    count = count + 1;
     
-    if(count >5) {
+    if(count >4) {
 
         alert("5회이상 안됨");
         
     }else {
+   
 
     let d = document.createElement("div");
     let c = document.createAttribute("class");
     c.value = "mb-3";
+    d.setAttributeNode(c);
+    c = document.createAttribute("id");
+    c.value = "file"+idx;
     d.setAttributeNode(c);
     
     //let d2 = document.createElement("div");
@@ -45,14 +51,51 @@ fileAdd.addEventListener("click",function() {
     i.setAttributeNode(n);
     i.setAttributeNode(id);
 
-    l.appendChild(i);
+    d.appendChild(i);
+    let i1 = document.createElement("input");
+    let b = document.createAttribute("type");
+    b.value = "button";
+    let c3 = document.createAttribute("class");
+    c3.value = "del btn-danger";
+    let t1 = document.createTextNode("삭제");
+    i1.setAttributeNode(b);
+    i1.setAttributeNode(c3);
+    i1.appendChild(t1);
+
+    buttonAttr = document.createAttribute("title");
+    buttonAttr.value = idx;
+    i1.setAttributeNode(buttonAttr);
+
+
+    d.appendChild(i1);
+
 
     addFiles.appendChild(d);
 
-    
+    count++;
+    idx++;
     }
     
-    
-    
 });
+
+addFiles.addEventListener("click",function(event){
+
+    let button = event.target;
+
+    if(button.classList[0] == 'del') {
+        document.getElementById("file" + button.title).remove();
+        count--;
+        }
+
+    /*
+    for(let j=0; j<5;j++) {
+        if(event.target.title = j ) {
+               addFiles.removeChild(event.target.id =="file"+j);
+        }
+    }
+
+    */
+     
+});
+
 
